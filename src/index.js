7c6173fdba0aefe9ae5638fea8ba5840a8a3e8ab
nicolezhao16 <nzhao16@seas.upenn.edu>
2022-11-13 17:36:19 -0500

    retru

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses"; // import expenses
+import Invoices from "./routes/invoices"; // import invoices
+import Invoice from "./routes/invoice"; // import invoice
+import Expense from "./routes/expense"; // import expense
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* nest route */}
+      <Route path="/" element={<App />} >
+        {/* add expenses component */}
+        <Route path="expenses" element={<Expenses />} >
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an expense</p>
+              </main>
+            }
+          />
+          <Route path=":expenseId" element = {<Expense />} />
+        </Route> 
+        {/* add invoices component */}
+        <Route path="invoices" element={<Invoices />} >
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        {/* catch all other routes */}
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>Page not available!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
